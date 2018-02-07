75195f2087fceee154835619c890deecbbace411
vineshtv <tv.vinesh@gmail.com>
2018-02-07 20:49:15 +0530

    Added some more Matrix functions.

--- NeuralNet/matrix.js
+++ NeuralNet/matrix.js
@@ -12,10 +12,14 @@ class Matrix{
         }
     }
     
-    randomise(){
+    randomise(dummy=false){
         for(let i = 0; i < this.rows; i++){
             for(let j = 0; j < this.cols; j++){
-                this.data[i][j] = Math.random() * 2 - 1;
+                if(dummy){
+                    this.data[i][j] = Math.floor(Math.random() * 10);
+                }else{
+                    this.data[i][j] = Math.random() * 2 - 1;
+                }
             }
         }
     }
@@ -34,6 +38,50 @@ class Matrix{
                 }
             }
         }
+    }
+    
+    static identity(n){
+        let result = new Matrix(n,n);
+        for(let i = 0; i < n; i++){
+            for(let j = 0; j < n; j++){
+                if(i == j){
+                    result.data[i][j] = 1;
+                }else{
+                    result.data[i][j] = 0;
+                }
+            }
+        }
+        return result;
+    }
+    
+    static multiply(a, b){
+        if(a.cols != b.rows){
+            console.log("Incompatible matrices");
+            return undefined;
+        }
+        
+        let result = new Matrix(a.rows, b.cols);
+        for(let i = 0; i < result.rows; i++){
+            for(let j = 0; j < result.cols; j++){
+                let sum = 0;
+                for(let k = 0; k < a.cols; k++){
+                    sum += a.data[i][k] * b.data[k][j];
+                }
+                result.data[i][j] = sum;
+            }
+        }
+        
+        return result;
+    }
+    
+    static transpose(a){
+        let result = new Matrix(a.cols, a.rows);
+        for(let i = 0; i < a.rows; i++){
+            for(let j = 0; j < a.cols; j++){
+                result.data[j][i] = a.data[i][j];
+            }
+        }
+        return result;
     }
     
     //Scalar multiply
--- NeuralNet/sketch.js
+++ NeuralNet/sketch.js
@@ -1,5 +1,20 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	//createCanvas(windowWidth, windowHeight);
+    let a = new Matrix(3,4);
+    
+    a.randomise(true);
+    console.table(a.data);
+    let b = Matrix.transpose(a);
+    console.table(b.data);
+    
+    let c = Matrix.multiply(a,b);
+    console.table(c.data);
+    
+    let d = Matrix.multiply(c, Matrix.identity(c.cols));
+    console.table(d.data);
+    
+    console.table(Matrix.identity(5).data);
+    
 }
 
 function draw() {
